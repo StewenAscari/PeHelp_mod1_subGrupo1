06dc39d195b0d3d30b23e43f686a7dc7ede7d37a
Stewen Ascari <asc.stewen18@gmail.com>
2017-02-20 11:31:40 -0300

    Teste classe principal.

---
+++ https:/github.com/StewenAscari/PeHelp_mod1_subGrupo1.git/Test/src/Juciano_Stewen_Igo.java
@@ -0,0 +1,9 @@
+
+public class Juciano_Stewen_Igo {
+
+	public static void main(String[] args) {
+		System.out.println("oi");
+
+	}
+
+}
---
+++ https:/github.com/StewenAscari/PeHelp_mod1_subGrupo1.git/Test/src/Stewen_juciano_Igo.java
@@ -0,0 +1,16 @@
+
+public class Stewen_juciano_Igo {
+	
+	public static void main(String []args){
+		String name1= "Stewen";	
+		String name2= "Juciano";
+		String name3= "Igo";
+		
+		System.out.println(name1+name2+name3);
+			
+		
+		
+		
+	}
+
+}
